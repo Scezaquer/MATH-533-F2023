fde6220d4414e2208449a8497a1069787561074a
Scezaquer <aurelien.buck.kaeffer@gmail.com>
2023-11-24 20:18:24 -0500

    [WIP] anova

--- src/raanova/ANOVA/anova.py
+++ src/raanova/ANOVA/anova.py
@@ -12,6 +12,7 @@ class ANOVA:
         self._wss = 0
         self._n = 0
         self._g = 0
+        self._ybar = 0
 
     def fit(self,
             X: npt.NDArray[np.int32], Y: npt.NDArray[np.float32],
@@ -19,6 +20,7 @@ class ANOVA:
 
         # TODO: update the attributes
         # TODO: check intercept works
+        # TODO: add guards
 
         # Add a column of ones to the input data for the intercept term
         if intercept:
@@ -26,6 +28,10 @@ class ANOVA:
 
         # Calculate the coefficients using the normal equation
         self._betas = np.sum(X*Y[:, np.newaxis], 0)/np.sum(X, 0)
+        self._ybar = np.average(Y)
+
+        self._tss = sum([(y - self._ybar)**2 for y in Y])
+        self._wss = sum([])
 
         return self._betas
 
