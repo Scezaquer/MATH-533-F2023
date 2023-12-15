c2f818355016091873ade56623aa59ef4d6b2f19
Scezaquer <aurelien.buck.kaeffer@gmail.com>
2023-12-15 18:21:51 -0500

    Revert "Merge branch 'ridge-lasso' into aurelien"
    This reverts commit 361f9dd4d24cf1d7fd265eef4112f19ef60a5032, reversing
    changes made to ed0f816ee921a15f113d0c8070ad9b68b1778778.

--- src/raanova/linear_regression/ridge.py
+++ src/raanova/linear_regression/ridge.py
@@ -1,29 +1,8 @@
-import numpy as np
-import numpy.typing as npt
-
 from .linear_regression import LinearRegression
-from .helper_functions import get_residuals, get_variance, get_r_squared
-from .helper_functions import get_ridge_hat_ann_matrix, get_OLS_CI
-
 
 class Ridge(LinearRegression):
     def __init__(self):
-        super().__init__()
-        self._penalty = 0
+        pass
 
-    def fit(self, X: list[list[float]], Y: list[float], intercept: bool = True, penalty: float = 0.1) -> list[float]:
-        if intercept:
-            X = np.column_stack((np.ones(X.shape[0]), X))
-
-        n, p = np.shape(X)
-        self._betas = np.linalg.inv(X.T @ X + self._penalty * np.identity(p)) @ (X.T @ Y)
-
-        self._residuals = get_residuals(X, Y, self._betas)
-        self._sigma_naive, self._sigma_corrected = get_variance(n, p, self._residuals)
-        self._rsquared = get_r_squared(n, Y, self._sigma_naive)
-        self._hat, self._annihilator = get_ridge_hat_ann_matrix(X, self._penalty)
-
-        return self._betas
-
-    def predict(self, X: npt.NDArray[np.float32]) -> npt.NDArray[np.float32]:
+    def fit(self, X: list[list[float]], Y: list[float]) -> list[float]:
         pass
